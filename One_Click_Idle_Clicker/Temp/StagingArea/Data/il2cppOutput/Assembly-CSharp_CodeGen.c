46b7e26f7cd56678cac972d29c613c9d18f8039d
Will M <willmcb234@gmail.com>
2019-10-17 13:30:15 -0400

    10.17.2019 Update 2.0

---
+++ One_Click_Idle_Clicker/Temp/StagingArea/Data/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,103 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void ScoreTracking::Start()
+extern void ScoreTracking_Start_mE94DA2B49B9ED2C123D3EF7C2EFECA83C8C6DD43 ();
+// 0x00000002 System.Void ScoreTracking::MainButtonClick()
+extern void ScoreTracking_MainButtonClick_m7BF4BDC01F761B29378CD0D1BA11435F7244E9DF ();
+// 0x00000003 System.Void ScoreTracking::OnClick1()
+extern void ScoreTracking_OnClick1_m201CE1E070358804A9E0CD370EAECBA6C2667937 ();
+// 0x00000004 System.Void ScoreTracking::OnClick5()
+extern void ScoreTracking_OnClick5_m30390031401C6B3B8929F5084364DA248B3E71C0 ();
+// 0x00000005 System.Void ScoreTracking::OnClick10()
+extern void ScoreTracking_OnClick10_m309A45C8E615D38F1857885E57BD1700F51AC007 ();
+// 0x00000006 System.Void ScoreTracking::OnClick100()
+extern void ScoreTracking_OnClick100_m2F728015C6B1E89B93B6E928401B42F13788F7CD ();
+// 0x00000007 System.Void ScoreTracking::IClick1()
+extern void ScoreTracking_IClick1_m24C4571C494C5F6B0DFEAD0156BE223A301AEC6C ();
+// 0x00000008 System.Void ScoreTracking::IClick5()
+extern void ScoreTracking_IClick5_m89CF2F1A36B605721A5E76BE923BE486097A0BEC ();
+// 0x00000009 System.Void ScoreTracking::IClicks10()
+extern void ScoreTracking_IClicks10_m6EBFCE9D382545E0570B60F0607BACF19C21E233 ();
+// 0x0000000A System.Void ScoreTracking::IClicks100()
+extern void ScoreTracking_IClicks100_m1F079D5B22EA9FEF03D8FD2092217E55C7AF303F ();
+// 0x0000000B System.Void ScoreTracking::IClicks500()
+extern void ScoreTracking_IClicks500_m286660C86B199A86C8F1D5D95ABC97B887C1A460 ();
+// 0x0000000C System.Void ScoreTracking::IClicks1000()
+extern void ScoreTracking_IClicks1000_m1495B5E5CA0956BF30CBD3E7158F091770AB3E9A ();
+// 0x0000000D System.Void ScoreTracking::ReSet()
+extern void ScoreTracking_ReSet_m398D4BEDA4FA1AFEECFE6078CFD589D99658ED1A ();
+// 0x0000000E System.Void ScoreTracking::Update()
+extern void ScoreTracking_Update_mD001D3EE15BD0D3ADFD8C2632FFBE6C1BE793E5F ();
+// 0x0000000F System.Void ScoreTracking::.ctor()
+extern void ScoreTracking__ctor_mFE5592846DA12AC89327AD4D8A40992D45E96D41 ();
+static Il2CppMethodPointer s_methodPointers[15] = 
+{
+	ScoreTracking_Start_mE94DA2B49B9ED2C123D3EF7C2EFECA83C8C6DD43,
+	ScoreTracking_MainButtonClick_m7BF4BDC01F761B29378CD0D1BA11435F7244E9DF,
+	ScoreTracking_OnClick1_m201CE1E070358804A9E0CD370EAECBA6C2667937,
+	ScoreTracking_OnClick5_m30390031401C6B3B8929F5084364DA248B3E71C0,
+	ScoreTracking_OnClick10_m309A45C8E615D38F1857885E57BD1700F51AC007,
+	ScoreTracking_OnClick100_m2F728015C6B1E89B93B6E928401B42F13788F7CD,
+	ScoreTracking_IClick1_m24C4571C494C5F6B0DFEAD0156BE223A301AEC6C,
+	ScoreTracking_IClick5_m89CF2F1A36B605721A5E76BE923BE486097A0BEC,
+	ScoreTracking_IClicks10_m6EBFCE9D382545E0570B60F0607BACF19C21E233,
+	ScoreTracking_IClicks100_m1F079D5B22EA9FEF03D8FD2092217E55C7AF303F,
+	ScoreTracking_IClicks500_m286660C86B199A86C8F1D5D95ABC97B887C1A460,
+	ScoreTracking_IClicks1000_m1495B5E5CA0956BF30CBD3E7158F091770AB3E9A,
+	ScoreTracking_ReSet_m398D4BEDA4FA1AFEECFE6078CFD589D99658ED1A,
+	ScoreTracking_Update_mD001D3EE15BD0D3ADFD8C2632FFBE6C1BE793E5F,
+	ScoreTracking__ctor_mFE5592846DA12AC89327AD4D8A40992D45E96D41,
+};
+static const int32_t s_InvokerIndices[15] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	15,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
